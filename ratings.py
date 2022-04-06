d2063a640b3a28d168523d65a4943b4944023060
Sean Fagan <faganseanp@gmail.com>
2022-04-06 11:46:50 -0600

    commit

---
+++ ratings.py
@@ -0,0 +1,28 @@
+"""Restaurant rating lister."""
+
+
+# put your code here
+open_file = open("scores.txt")
+rating_dict = {}
+
+for line in open_file:
+    line = line.rstrip('\n').split(':')
+    rating_dict[line[0]] = int(line[1])
+    
+# print(rating_dict)
+def sort_ratings():
+    each = rating_dict.items()
+    sorts = sorted(each)
+    for ea in sorts:
+        print(ea[0], "is rated at", ea[1])
+    rate_more = input("Do you want to rate another restaurant? Type y or n:\n")
+    if rate_more == "y":
+        user_input()
+    
+def user_input():
+    rest = input("What is the name of the restaurant:\n")
+    rating = int(input("Rate the restaurant from 1-5:\n"))
+    rating_dict[rest] = rating
+    sort_ratings()
+
+user_input()
